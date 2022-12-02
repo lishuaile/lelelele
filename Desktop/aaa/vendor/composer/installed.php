508ab3527d3b7634f849e11ab6ce9fb5a5cd1160
李帅乐 <1573760863@qq.com>
2022-12-02 19:09:22 +0800

    1

---
+++ Desktop/aaa/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9aa6f475305e2fa2cfb0c4279c02085415531a00',
+        'name' => 'lishuaile/lele',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lishuaile/lele' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9aa6f475305e2fa2cfb0c4279c02085415531a00',
+            'dev_requirement' => false,
+        ),
+    ),
+);
